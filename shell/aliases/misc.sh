3aad83869b6c0671f06edc34d7406d40707f4dea
Jose Luis Salas <josacar@gmail.com>
2013-12-23 19:36:55 +0100

    Add z and shell aliases

--- shell/aliases/misc.sh
+++ shell/aliases/misc.sh
@@ -1,5 +1,12 @@
 alias less='less -r'
-alias ack='ack-grep'
+
+which ack-grep > /dev/null
+[ $? -eq 0 ] && alias ack='ack-grep'
+
 alias ber='bundle exec rspec'
 alias be='bundle exec'
 alias vless='/usr/share/vim/vim73/macros/less.sh'
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
