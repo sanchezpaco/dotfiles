b2b4bd8e9e122edcf2ed95f2931acc071499d169
Jose Luis Salas <josacar@users.noreply.github.com>
2016-04-06 14:09:02 +0000

    nvim as vim if installed

--- shell/aliases/misc.sh
+++ shell/aliases/misc.sh
@@ -3,6 +3,9 @@ alias less='less -r'
 which ack-grep > /dev/null
 [ $? -eq 0 ] && alias ack='ack-grep'
 
+which nvim > /dev/null
+[ $? -eq 0 ] && alias vim='nvim'
+
 alias vless='/usr/share/vim/vim73/macros/less.sh'
 alias ..="cd .."
 alias ...="cd ../.."
