275c44c5563a0871c0c6d8cf943767450ffba099
Jose Luis Salas <josacar@users.noreply.github.com>
2017-04-14 11:09:19 +0200

    Add custom slack status for pomdoros

--- bin/pomodoro-start.sh
+++ bin/pomodoro-start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export PATH=$PATH:/usr/local/bin
 source $HOME/.slack-api-token
 curl "https://slack.com/api/users.setPresence?token=${SLACK_API_TOKEN}&presence=away" -s
 curl "https://slack.com/api/dnd.setSnooze?token=${SLACK_API_TOKEN}&num_minutes=25"
+curl "https://slack.com/api/users.profile.get?token=${SLACK_API_TOKEN}" -s | jq '.profile | @uri'  | tr -d '"' > $TMPDIR/slack-profile.txt
+curl "https://slack.com/api/users.profile.set?token=${SLACK_API_TOKEN}&profile=%7B%22status_text%22%3A%22In%20a%20pomodoro.%20Reach%20me%20in%2025%20minutes.%22%2C%22status_emoji%22%3A%22%3Atomato%3A%22%7D"
--- bin/pomodoro-stop.sh
+++ bin/pomodoro-stop.sh
@@ -3,3 +3,4 @@
 source $HOME/.slack-api-token
 curl "https://slack.com/api/users.setPresence?token=${SLACK_API_TOKEN}&presence=auto"
 curl "https://slack.com/api/dnd.endSnooze?token=${SLACK_API_TOKEN}"
+curl "https://slack.com/api/users.profile.set?token=${SLACK_API_TOKEN}&profile=$(cat $TMPDIR/slack-profile.txt)"
