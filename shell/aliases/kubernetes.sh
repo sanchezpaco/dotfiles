ab827a979f848c3cedb6817b69f2c394de2391f8
Paco Sanchez <sanchezpaco@Pacos-MacBook-Pro.local>
2017-04-27 21:25:34 +0200

    Adds new aliases for terraform and kubernetes

---
+++ shell/aliases/kubernetes.sh
@@ -0,0 +1,11 @@
+alias kget='kubectl get'
+alias kdescribe='kubectl describe'
+alias kdelete='kubectl delete'
+alias kedit='kubectl edit'
+alias kapply='kubectl apply'
+alias kcreate='kubectl create'
+alias k='kubectl'
+alias klogs='kubectl logs'
+alias kconfig='kubectl config'
+
+
---
+++ shell/aliases/terraform.sh
@@ -0,0 +1,4 @@
+alias tplan='terraform plan'
+alias tapply='terraform apply'
+alias tdestroy='terraform destroy'
+alias tget='terraform get'
